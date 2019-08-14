098f9cfbebba824723358029875cbbe85eb28202
bhanu velamuri <35382776+bhanuvelamuri@users.noreply.github.com>
2019-08-14 01:27:33 -0400

    update

--- src/test/java/Header/Title.java
+++ src/test/java/Header/Title.java
@@ -4,30 +4,26 @@ import java.io.IOException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.testng.AssertJUnit;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.model.Log;
 
 import resources.utilities;
 
 public class Title extends utilities {
 
-		public static Logger Log = LogManager.getLogger(ValidateTitle.class.getName());
 		@BeforeTest
 		public void Initilize() throws IOException {
 			driver = initializeDriver();
-			Log.info("Driver is initilized");
 			driver.get(prop.getProperty("url"));
-			Log.info("Navigated to Homepage");
 		}
 		@Test
 		public void basePageNavigation() throws IOException {
 			ValidateTitle l = new ValidateTitle(driver);
 			//compare the text from the browser 
-			AssertJUnit.assertEquals(l.getTitle().getText(), "EXCITE SERVICES | Web design, development, E-commerce and SEO");
-			AssertJUnit.assertTrue(l.getTitle().isDisplayed());
-			Log.info("Successfully Validated Text Message");
+			l.getTitle().equals("EXCITE SERVICES | Web design, development, E-commerce and SEO");
 		}
 		@AfterTest
 		public void teardown() {
