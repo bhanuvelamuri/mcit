0df383d595885e61f4c75976434eaf94088a43fb
bhanu velamuri <35382776+bhanuvelamuri@users.noreply.github.com>
2019-08-14 00:18:33 -0400

    added utilities

--- src/main/java/mcitproject/home.java
+++ src/main/java/mcitproject/home.java
@@ -7,10 +7,7 @@ public class home {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.gecko.driver", "/bhanu/geckodriver");
-		WebDriver driver = new FirefoxDriver();
-		driver.get("https://exciteservices.net");
-		driver.close();
+		
 	}
 
 }
