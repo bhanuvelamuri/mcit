7c053c9f7608e0fd3a7527d4dfea12a20a83d994
bhanu velamuri <35382776+bhanuvelamuri@users.noreply.github.com>
2019-08-13 21:56:00 -0400

    upload

---
+++ src/main/java/mcitproject/home.java
@@ -0,0 +1,16 @@
+package mcitproject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class home {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.gecko.driver", "/bhanu/geckodriver");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://exciteservices.net");
+		driver.close();
+	}
+
+}
